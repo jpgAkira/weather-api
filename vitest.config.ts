324e6cf4cd527ddb2e449d80c028061a6aa8054c
Guilherme <157712119+jpgAkira@users.noreply.github.com>
2025-11-10 16:28:58 -0300

    initial setup

---
+++ vitest.config.ts
@@ -0,0 +1,46 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from "vitest/config";
+
+import path from "path";
+export default defineConfig({
+  test: {
+    globals: true,
+    fileParallelism: false,
+    setupFiles: ["vitest.setup.ts"],
+    globalSetup: ["vitest.global.setup.ts"],
+    include: ["src/**/*.{spec,test}.{ts}"],
+    testTimeout: 10000,
+    coverage: {
+      reportsDirectory: "./coverage",
+      provider: "v8",
+      include: ["src/**/*.{ts}"],
+      exclude: [
+        "**/*.test.{ts}",
+        "**/*.spec.{ts}",
+
+        "**/types/**",
+        "**/*.d.ts",
+        "**/*.type.{ts,ts}",
+        "**/*.types.{ts,ts}",
+        "**/*.contract.{ts,ts}",
+        "**/*.contract.*{ts,ts}",
+        "**/*.protocol.{ts,ts}",
+        "**/*.interface.{ts,ts}",
+        "**/*.mock.{ts,tsx}",
+        "**/*.mocks.{ts,tsx}",
+        "**/mocks/**",
+        "**/__mocks__/**",
+        "**/__tests__/**",
+        "**/__test-utils__/**",
+        "**/*.test-util.ts",
+      ],
+    },
+  },
+  resolve: {
+    alias: {
+      "@src": path.resolve(__dirname, "src"),
+      "@test": path.resolve(__dirname, "test"),
+    },
+  },
+});
