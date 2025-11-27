7d0627561ca213af72f1a850fc59bc48ccd9091b
Guilherme <157712119+jpgAkira@users.noreply.github.com>
2025-11-27 08:58:05 -0300

    test: encryption of auth service

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -3,6 +3,7 @@ import { AuthService } from './auth.service.js';
 
 describe('AuthService', () => {
   let service: AuthService;
+  const defaultPassword = 'test';
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -14,5 +15,20 @@ describe('AuthService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should create a password with encryption', async () => {
+    const hashedPasswordRegex = /\$2b\$10\$.+/;
+
+    const hash = await service.hashingPassword(defaultPassword);
+
+    expect(hash).toMatch(hashedPasswordRegex);
+  });
+
+  it('should compare a hashed password with a password informed by user', async () => {
+    const hash = await service.hashingPassword(defaultPassword);
+
+    expect(await service.isMatchPassword(defaultPassword, hash)).toBeTruthy();
+    expect(await service.isMatchPassword('1234', hash)).toBeFalsy();
   });
 });
