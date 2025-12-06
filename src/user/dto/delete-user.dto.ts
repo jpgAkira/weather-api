e3567e223a85653beb6d8ca84a43a5bf5591df0e
Guilherme <157712119+jpgAkira@users.noreply.github.com>
2025-12-06 16:14:22 -0300

    feat: finish crud

---
+++ src/user/dto/delete-user.dto.ts
@@ -0,0 +1,4 @@
+export class deleteUserResponseDto {
+  message: string;
+  userId: string;
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,20 @@
-import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Inject,
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto.js';
-// import { UpdateUserDto } from './dto/update-user.dto.js';
+import { UpdateUserDto, UpdateUserResponseDto } from './dto/update-user.dto.js';
 import { PrismaService } from '../prisma/prisma.service.js';
 import { UserPublicDto } from './dto/user-public.dto.js';
-import { LoginUserDto, LoginUserResponseDto } from './dto/login-user.dto.js';
+import { LoginUserDto } from './dto/login-user.dto.js';
+import { AuthResponseDto } from './dto/auth-user.dto.js';
 import { AuthService } from '../auth/auth.service.js';
+import { Prisma } from '../generated/prisma/client.js';
+import { PrismaErrorCode } from '../prisma/prisma.types.js';
+import { deleteUserResponseDto as DeleteUserResponseDto } from './dto/delete-user.dto.js';
 
 @Injectable()
 export class UserService {
@@ -13,97 +23,139 @@ export class UserService {
   @Inject()
   private readonly authService: AuthService;
 
-  async create(createUserDto: CreateUserDto): Promise<UserPublicDto> {
-    const hashedPassword = await this.authService.hashingPassword(
-      createUserDto.password,
-    );
+  async create(createUserDto: CreateUserDto): Promise<AuthResponseDto> {
+    try {
+      const hashedPassword = await this.authService.hashingPassword(
+        createUserDto.password,
+      );
 
-    const newUser = await this.db.user.create({
-      data: { ...createUserDto, password: hashedPassword },
-      omit: {
-        password: true,
-        createdAt: true,
-        updatedAt: true,
-      },
-    });
+      const newUser = await this.db.user.create({
+        data: { ...createUserDto, password: hashedPassword },
+        omit: {
+          email: true,
+          name: true,
+          password: true,
+          createdAt: true,
+          updatedAt: true,
+        },
+      });
 
-    const token = await this.authService.createToken(newUser.id);
+      const token = await this.authService.createToken(newUser.id);
 
-    return {
-      ...newUser,
-      token,
-    };
-  }
+      return { token };
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === PrismaErrorCode.UNIQUE_CONSTRAINT) {
+          throw new BadRequestException('O e-mail informado já está em uso');
+        }
 
-  async login(loginUserDto: LoginUserDto): Promise<LoginUserResponseDto> {
-    const user = await this.db.user.findUnique({
-      where: { email: loginUserDto.email },
-      include: {
-        citys: true,
-      },
-      omit: {
-        createdAt: true,
-        updatedAt: true,
-      },
-    });
-
-    if (
-      !user ||
-      !(await this.authService.isMatchPassword(
-        loginUserDto.password,
-        user.password,
-      ))
-    ) {
-      throw new UnauthorizedException(
-        'Verifique seus dados e tente novamente.',
+        if (error.code === PrismaErrorCode.REQUIRED_PATH) {
+          throw new BadRequestException(
+            'Por favor informe todos os campos obrigatórios',
+          );
+        }
+      }
+      throw new InternalServerErrorException(
+        'Ocorreu um erro inesperado, por favor tente novamente mais tarde',
       );
     }
-
-    const token = await this.authService.createToken(user.id);
-
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const { password, ...responseUser } = user;
-
-    return {
-      ...responseUser,
-      token,
-    };
   }
 
-  async findAll(): Promise<UserPublicDto[]> {
-    return await this.db.user.findMany({
-      omit: {
-        password: true,
-        createdAt: true,
-        updatedAt: true,
-        email: true,
-      },
-    });
+  async login(loginUserDto: LoginUserDto): Promise<AuthResponseDto> {
+    try {
+      const user = await this.db.user.findUnique({
+        where: { email: loginUserDto.email },
+        include: {
+          citys: true,
+        },
+        omit: {
+          createdAt: true,
+          updatedAt: true,
+        },
+      });
+
+      if (
+        !user ||
+        !(await this.authService.isMatchPassword(
+          loginUserDto.password,
+          user.password,
+        ))
+      ) {
+        throw new Error('Verifique seus dados e tente novamente.');
+      }
+
+      const token = await this.authService.createToken(user.id);
+
+      return { token };
+    } catch (error) {
+      throw new UnauthorizedException((error as Error).message);
+    }
   }
 
-  async findOne(id: string): Promise<UserPublicDto | { message: string }> {
-    const user = await this.db.user.findUnique({
-      where: { id },
-      omit: {
-        password: true,
-        createdAt: true,
-        updatedAt: true,
-        email: true,
-      },
-    });
+  async findProfile(id: string): Promise<UserPublicDto> {
+    try {
+      const user = await this.db.user.findUnique({
+        where: { id },
+        omit: {
+          password: true,
+          createdAt: true,
+          updatedAt: true,
+        },
+      });
 
-    return !user
-      ? {
-          message: 'Nenhum usuário foi encontrado.',
+      if (!user) {
+        throw new Error('Token inválido');
+      }
+
+      return user;
+    } catch (error) {
+      throw new UnauthorizedException(error);
+    }
+  }
+
+  async update(
+    id: string,
+    updateUserDto: UpdateUserDto,
+  ): Promise<UpdateUserResponseDto> {
+    try {
+      if (updateUserDto.password) {
+        const hashPassword = await this.authService.hashingPassword(
+          updateUserDto.password,
+        );
+
+        updateUserDto = { ...updateUserDto, password: hashPassword };
+      }
+
+      const updatedUser = await this.db.user.update({
+        where: { id },
+        data: updateUserDto,
+        omit: {
+          createdAt: true,
+          updatedAt: true,
+          password: true,
+        },
+      });
+
+      return { user: updatedUser, message: 'Atualizado com sucesso.' };
+    } catch (error) {
+      if (error instanceof Prisma.PrismaClientKnownRequestError) {
+        if (error.code === PrismaErrorCode.MISSING_ID) {
+          throw new UnauthorizedException('Informe um id válido.');
         }
-      : user;
+      }
+      throw new InternalServerErrorException(
+        'Ocorreu um erro inesperado, por favor tente novamente mais tarde',
+      );
+    }
   }
 
-  // async update(id: string, updateUserDto: UpdateUserDto) {
-  //   return `This action updates a #${id} user`;
-  // }
+  async remove(id: string): Promise<DeleteUserResponseDto> {
+    try {
+      await this.db.user.delete({ where: { id } });
 
-  // async remove(id: string) {
-  //   return `This action removes a #${id} user`;
-  // }
+      return { message: 'Usuário deletado com sucesso.', userId: id };
+    } catch {
+      throw new UnauthorizedException('Informe um id válido.');
+    }
+  }
 }
