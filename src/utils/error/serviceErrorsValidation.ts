9b75e31f6416bc748ea8576766f33be9ca418565
Guilherme <157712119+jpgAkira@users.noreply.github.com>
2025-12-22 23:26:54 -0300

    refactor: implement error handling to request errors

--- src/utils/error/serviceErrorsValidation.ts
+++ src/utils/error/serviceErrorsValidation.ts
@@ -1,14 +1,36 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import {
+  BadRequestException,
+  HttpException,
   InternalServerErrorException,
+  NotFoundException,
   UnauthorizedException,
 } from '@nestjs/common';
 import { Prisma } from '../../generated/prisma/client.js';
 import { PrismaErrors } from '../../prisma/prisma.types.js';
+import * as HTTPUtil from '../request.js';
 
 export class ServiceErrorValidation {
-  public static tratament(error: unknown) {
+  public static tratament(error: unknown): never {
     if (error instanceof Prisma.PrismaClientKnownRequestError) {
       this.identifyPrismaError(error);
+    }
+
+    if (HTTPUtil.Request.isRequestError(error)) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      const { data, status } = HTTPUtil.Request.extractErrorData(error);
+
+      if (status === 400) {
+        throw new BadRequestException(data?.message);
+      }
+
+      if (status === 404) {
+        throw new NotFoundException(data?.message);
+      }
+
+      if (status === 429) {
+        throw new HttpException(`${data?.message}`, status);
+      }
     }
 
     if (error instanceof Error) {
