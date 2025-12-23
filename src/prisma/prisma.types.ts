d5ee69bd9875c639391b699077c59d40844893ad
Guilherme <157712119+jpgAkira@users.noreply.github.com>
2025-12-22 23:31:32 -0300

    refactor: implement default errors

--- src/prisma/prisma.types.ts
+++ src/prisma/prisma.types.ts
@@ -1,3 +1,28 @@
+import { BadRequestException, UnauthorizedException } from '@nestjs/common';
+
+export const PrismaErrors = [
+  {
+    code: 'P2012',
+    function() {
+      throw new BadRequestException(
+        'Por favor informe todos os campos obrigatórios',
+      );
+    },
+  },
+  {
+    code: 'P2002',
+    function() {
+      throw new BadRequestException('O e-mail informado já está em uso');
+    },
+  },
+  {
+    code: 'P2025',
+    function() {
+      throw new UnauthorizedException('Informe um id válido.');
+    },
+  },
+];
+
 export const PrismaErrorCode = {
   REQUIRED_PATH: 'P2012',
   UNIQUE_CONSTRAINT: 'P2002',
