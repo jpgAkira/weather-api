4ed14aa95d26ef336e5e9dce25f24d820601395e
Guilherme <157712119+jpgAkira@users.noreply.github.com>
2025-12-22 23:33:20 -0300

    refactor: removing useless things

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller.js';
-import { AppService } from './app.service.js';
 import { UserModule } from './user/user.module.js';
 import { PrismaService } from './prisma/prisma.service.js';
 import { AuthModule } from './auth/auth.module.js';
+import { CityModule } from './city/city.module.js';
 
 @Module({
-  imports: [UserModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService, PrismaService],
+  imports: [UserModule, AuthModule, CityModule],
+  providers: [PrismaService],
 })
 export class AppModule {}
